c919de83ea70fbf9084b691a4091253855f5705a
Mohamed Ibrahim Abdulghani <85433483+MohamedIbrahimAbdulghani@users.noreply.github.com>
2025-04-14 09:09:32 +0200

    Error-> Wrong single responsability principle

---
+++ classes/MatchClass.php
@@ -0,0 +1,13 @@
+<?php
+
+class MatchClass {
+    public function attack() {
+        echo "this is attack function";
+    }
+    public function defense() {
+        echo "this is defense function";
+    }
+    public function keeper() {
+        echo "this is keeper function";
+    }
+}
